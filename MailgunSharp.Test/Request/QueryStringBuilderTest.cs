8dba88bd7afc370db9c38917a96f7f0373e84c43
Jonathan Throne <jonathan.throne@brmsonline.com>
2018-07-19 08:49:26 -0700

    Added in first tests for QueryString util class.

---
+++ MailgunSharp.Test/Request/QueryStringBuilderTest.cs
@@ -0,0 +1,53 @@
+using System;
+using MailgunSharp.Request;
+using Xunit;
+
+namespace MailgunSharp.Test.Request
+{
+  public class QueryStringBuilderTest
+  {
+    [Fact]
+    public void Initialize_QueryStringBuilder_Should_Return_Zero_Count_QueryString()
+    {
+      var queryStringBuilder = new QueryStringBuilder();
+
+      var queryString = queryStringBuilder.Build();
+
+      Assert.True(queryString != null);
+
+      Assert.True(queryString.Count == 0);
+    }
+
+    [Theory]
+    [InlineData("var1", "var2")]
+    [InlineData("a", "b")]
+    public void Append_Parameter_To_QueryString_Shoul_Return_True_And_Have_Count_One_QueryString(string variable, string value)
+    {
+      var queryStringBuilder = new QueryStringBuilder();
+
+      var queryString = queryStringBuilder
+                          .Append(variable, value)
+                          .Build();
+
+      Assert.True(queryString != null);
+
+      Assert.True(queryString.Count == 1);
+    }
+
+    [Fact]
+    public void Append_Multiple_Parameters_Count_Of_QueryString_Should_Be_The_Same_As_List_Of_Params()
+    {
+      var queryStringBuilder = new QueryStringBuilder();
+
+      var queryString = queryStringBuilder
+                          .Append("var1", "val1")
+                          .Append("var2", "val2")
+                          .Append("var3", "val3")
+                          .Build();
+
+      Assert.True(queryString != null);
+
+      Assert.True(queryString.Count == 3);
+    }
+  }
+}
---
+++ MailgunSharp.Test/Request/QueryStringTest.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+using MailgunSharp.Request;
+
+namespace MailgunSharp.Test.Request
+{
+  public class QueryStringTest
+  {
+    [Fact]
+    public void Initialized_QueryString_Should_Have_Count_Equal_To_Zero()
+    {
+      var queryString = new QueryString();
+
+      Assert.True(queryString != null);
+
+      Assert.True(queryString.Count == 0);
+    }
+
+    [Theory]
+    [InlineData("var1", "val1")]
+    public void QueryString_AppendParam_Check_If_Exists_Should_Have_Count_Equal_To_One(string variable, string value)
+    {
+      var queryString = new QueryString();
+
+      var wasAppended = queryString.AppendIfNotNullOrEmpty(variable, value);
+
+      Assert.True(wasAppended);
+
+      Assert.True(queryString.Count == 1);
+    }
+
+    [Theory]
+    [InlineData("", "var1")]
+    [InlineData("var1", "")]
+    [InlineData("", "")]
+    [InlineData(null, null)]
+    public void QueryString_AppendParam_Should_Throw_Error_If_Variable_Or_Value_Is_Empty_Or_Null(string variable, string value)
+    {
+      var queryString = new QueryString();
+
+      Assert.Throws<ArgumentNullException>(() => queryString.AppendIfNotNullOrEmpty(variable, value));
+    }
+  }
+}

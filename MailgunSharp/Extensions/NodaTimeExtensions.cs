32c54c387de4e9fb727fbd2798f9affc90e8cae2
Jonathan Throne <jonathan.throne@brmsonline.com>
2018-10-04 09:02:46 -0700

    Added in documentation for nodatime extension class.

--- MailgunSharp/Extensions/NodaTimeExtensions.cs
+++ MailgunSharp/Extensions/NodaTimeExtensions.cs
@@ -5,6 +5,11 @@ namespace MailgunSharp.Extensions
 {
   internal static class NodaTimeExtensions
   {
+    /// <summary>
+    ///  Turn a NodaTime instant into a datetime format of rfc2822 found on page #14.
+    /// </summary>
+    /// <param name="instant">The NodaTime instant to be used.</param>
+    /// <returns>A string representing a datetime in RFC2822 format.</returns>
     public static string ToRfc2822DateFormat(this Instant instant)
     {
       var pattern = InstantPattern.CreateWithInvariantCulture("ddd d MMM yyyy HH:mm:ss 'GMT'");
